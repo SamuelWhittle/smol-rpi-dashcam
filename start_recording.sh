56a3202cf3b27f46a91c0ba0bf3c1f41a9a1973c
SamuelWhittle <swbnp7@gmail.com>
2024-09-30 12:19:46 -0500

    improved start_recording, removed for loop for largest device finding

--- start_recording.sh
+++ start_recording.sh
@@ -5,9 +5,7 @@
 
 RUN_TIME="$(date '+%Y-%m-%d-%H-%M-%S')"
 RECORDING_STORAGE_MOUNT_PATH="$PWD/recording_storage_mount"
-FOUND_DEVICES=(`lsblk -bo NAME,SIZE,FSTYPE | grep -oP '(sd\w\d)\s+(\d+)\s+(\w+)' | grep -oP '(sd\w\d)\s+(\d+)'`)
-LARGEST_DEVICE=""
-LARGEST_SIZE=0
+LARGEST_DEVICE=`lsblk -bnro SIZE,NAME,FSTYPE | grep -oP '(\d+)\s+(sd\w\d)\s+(\w+)' | grep -oP '(\d+)\s+(sd\w\d)' | sort -nr | head -n 1 | awk '{print $2}'`
 
 # helper function to test if a mount succeeded
 mount_failed() {
@@ -15,18 +13,10 @@ mount_failed() {
 	test $MAYBE_MOUNT == 0
 }
 
-# find the largest block device
-for index in ${!FOUND_DEVICES[@]}; do
-	if !(( index % 2 )) && test ${FOUND_DEVICES[(( index + 1 ))]} -gt $LARGEST_SIZE; then
-		LARGEST_SIZE=${FOUND_DEVICES[(( index + 1 ))]}
-		LARGEST_DEVICE=${FOUND_DEVICES[$index]}
-	fi
-done
-
 # stop if we did not find a block device
 if [ "$LARGEST_DEVICE" == "" ]; then
-	echo "Could not find a block device to store video to."
-	echo "$(date '+%Y-%m-%d %H:%M:%S') : Could not find a block device to store video to." >> "$PWD/logs/run_logs.txt"
+	>&2 echo "Could not find a block device to store video to."
+	echo "$(date '+%Y-%m-%d %H:%M:%S') : Could not find a block device to store video to." > "$PWD/logs/run_logs.txt"
 	exit 1
 fi
 
@@ -42,7 +32,8 @@ sudo mount $LARGEST_DEVICE_PATH $RECORDING_STORAGE_MOUNT_PATH
 
 # stop if the mount failed
 if mount_failed "$LARGEST_DEVICE_PATH" "$RECORDING_STORAGE_MOUNT_PATH"; then
-	echo "$(date '+%Y-%m-%d %H:%M:%S') : Could not mount $LARGEST_DEVICE_PATH to $RECORDING_STORAGE_MOUNT_PATH" >> "$PWD/logs/run_logs.txt"
+	>&2 echo "$(date '+%Y-%m-%d %H:%M:%S') : Could not mount $LARGEST_DEVICE_PATH to $RECORDING_STORAGE_MOUNT_PATH"
+	echo "$(date '+%Y-%m-%d %H:%M:%S') : Could not mount $LARGEST_DEVICE_PATH to $RECORDING_STORAGE_MOUNT_PATH" > "$PWD/logs/run_logs.txt"
 	exit 1
 fi
 
@@ -50,4 +41,4 @@ echo "Successfully mounted $LARGEST_DEVICE_PATH to $RECORDING_STORAGE_MOUNT_PATH
 
 # start recording video to device
 echo "Starting recording to $RECORDING_STORAGE_MOUNT_PATH/$RUN_TIME-chunk%04d.h264"
-#rpicam-vid -c dashcam_config.txt -o recording_storage_mount/"${RUN_TIME}"-chunk%04d.h264
+rpicam-vid -c dashcam_config.txt -o recording_storage_mount/"${RUN_TIME}"-chunk%04d.h264
