ee1d7f0e82d293a28de2d5525f6c9f62cab7d565
SamuelWhittle <swbnp7@gmail.com>
2024-10-02 09:36:58 -0500

    fixed issue where the service could not start due to missing folders

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 sudo apt install rpicam-apps
+
+mkdir logs
+mkdir recording_storage_mount
+
+sudo chown -R $USER:$USER $PWD
+
 sudo sed "s,/path/to/working/dir,$PWD,g" ./dashcam.service > /lib/systemd/system/dashcam.service
 sudo systemctl daemon-reload
 sudo systemctl enable dashcam.service
