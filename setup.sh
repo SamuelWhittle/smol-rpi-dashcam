573d65b2bde3f41b08629755dca3b721b6c3f3f1
SamuelWhittle <swbnp7@gmail.com>
2024-10-02 13:21:01 -0500

    fixed issue where setup would chwqown working directory as root

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ sudo apt install rpicam-apps
 mkdir logs
 mkdir recording_storage_mount
 
-sudo chown -R $USER:$USER $PWD
+#USER=${SUDO_USER:-${USER}}
+
+#sudo chown -R $USER:$USER $PWD/recording_storage_mount
 
 sudo sed "s,/path/to/working/dir,$PWD,g" ./dashcam_cron_job > /etc/cron.d/dashcam_cron_job
 sudo sed "s,/path/to/working/dir,$PWD,g" ./dashcam.service > /lib/systemd/system/dashcam.service
